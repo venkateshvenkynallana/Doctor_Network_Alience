6b1839cec857f50380541e97533af9a2e7462fb7
venky <venkateshnallana2003@gmail.com>
2026-02-04 11:05:47 +0530

    UI conflict

--- src/components/doctorDashboard/Settings/Profile/Profile.jsx
+++ src/components/doctorDashboard/Settings/Profile/Profile.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from '../../../../context/AuthContext';
 
 
 
-const Profile = ({ onMenuClick }) => {
+const Profile = ({ onMenuClick }) =>  {
 
   
   const {updateProfile, authUser} = useContext(AuthContext);
