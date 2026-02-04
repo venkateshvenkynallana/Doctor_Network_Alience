02eb8fb8eb44301fb463579b8cd6f0e833dc2760
Nandiniboddeda <boddedanandini249@gmail.com>
2026-02-04 18:30:06 +0530

    backend code

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -4,8 +4,11 @@ import mongoose from "mongoose";
 export const connectDB = async()=>{
     try {
         mongoose.connection.on('connected',()=>console.log('DataBase connected'));
-        await mongoose.connect(`${process.env.MONGODB_URL}`)
-    } catch (error) {
-        console.log("mongodb error",error)
-    }
-}
+        await mongoose.connect(`${process.env.MONGODB_URL}`);
+        console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
